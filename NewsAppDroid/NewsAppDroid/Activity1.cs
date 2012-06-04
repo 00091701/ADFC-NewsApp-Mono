a91ca2ea0e04f91280ff01197fa841f33f3b52bf
David Hoffmann <adfc-newsapp@hoffmann-projects.de>
2012-06-04 17:23:32 +0200

    Neues Android Projekt angelegt.

---
+++ NewsAppDroid/NewsAppDroid/Activity1.cs
@@ -0,0 +1,56 @@
+/*
+ * This file is part of ADFC-NewsApp
+ * Copyright (C) 2012 David Hoffmann
+ *
+ * ADFC-NewsApp is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * ADFC-NewsApp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ADFC-NewsApp; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
+
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace de.dhoffmann.mono.adfcnewsapp.droid
+{
+	[Activity (Label = "NewsAppDroid", MainLauncher = true)]
+	public class Activity1 : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				button.Text = string.Format (
+					"{0} clicks!",
+					count++
+				);
+			};
+		}
+	}
+}
+
+
