85cbf74a071f58db1d6d35f598001236c4d9d40f
David Hoffmann <adfc-newsapp@hoffmann-projects.de>
2012-11-18 02:13:52 +0100

    Den Splashscreen habe ich wegen fehlender GPL2 Lizenz ausgetauscht.
    Auch wenn der Inhalt sehr ähnlich ist, so kommt der Inhalt dennoch aus dem Projekt
    https://github.com/00091701/FullscreenPresentation-Mono
    übernommen worden.
    
    Signed-off-by: David Hoffmann <adfc-newsapp@hoffmann-projects.de>

---
+++ NewsAppDroid/NewsAppDroid/Droid/Splash.cs
@@ -0,0 +1,48 @@
+/*
+ * This file is part of ADFC-NewsApp
+ * Copyright (C) 2012 David Hoffmann
+ *
+ * ADFC-NewsApp is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * ADFC-NewsApp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ADFC-NewsApp; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace de.dhoffmann.mono.adfcnewsapp.droid
+{
+	[Activity (Label = "SplashActivity", MainLauncher = true, NoHistory = true, Theme = "@style/Theme.Splash")]			
+	public class SplashActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			System.Threading.Thread.Sleep(1000);
+
+			StartActivity(typeof(Tabs));
+		}
+	}
+}
+
