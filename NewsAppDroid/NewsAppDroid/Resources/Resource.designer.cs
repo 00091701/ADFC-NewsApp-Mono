d7237de9b85ee35d8b647840ea2a4d0cd39b6f7f
David Hoffmann <adfc-newsapp@hoffmann-projects.de>
2012-11-18 02:03:29 +0100

    BugFix: Grauer Kasten Es wurde auf einigen Detailseiten ein grauer Kasten angezeigt.
    Signed-off-by: David Hoffmann <adfc-newsapp@hoffmann-projects.de>

--- NewsAppDroid/NewsAppDroid/Resources/Resource.designer.cs
+++ NewsAppDroid/NewsAppDroid/Resources/Resource.designer.cs
@@ -52,62 +52,62 @@ namespace de.dhoffmann.mono.adfcnewsapp.droid
 			// aapt resource value: 0x7f060006
 			public const int btnWebsite = 2131099654;
 			
-			// aapt resource value: 0x7f060013
-			public const int cbDataUpdate = 2131099667;
-			
 			// aapt resource value: 0x7f060012
-			public const int cbDateIndicate = 2131099666;
+			public const int cbDataUpdate = 2131099666;
 			
-			// aapt resource value: 0x7f060015
-			public const int cbFeedItem = 2131099669;
+			// aapt resource value: 0x7f060011
+			public const int cbDateIndicate = 2131099665;
+			
+			// aapt resource value: 0x7f060014
+			public const int cbFeedItem = 2131099668;
 			
 			// aapt resource value: 0x7f060003
 			public const int imageView1 = 2131099651;
 			
-			// aapt resource value: 0x7f060011
-			public const int linearLayout1 = 2131099665;
+			// aapt resource value: 0x7f060010
+			public const int linearLayout1 = 2131099664;
 			
 			// aapt resource value: 0x7f060002
 			public const int llHeader = 2131099650;
 			
-			// aapt resource value: 0x7f060014
-			public const int lvDataSubscription = 2131099668;
+			// aapt resource value: 0x7f060013
+			public const int lvDataSubscription = 2131099667;
 			
 			// aapt resource value: 0x7f060001
 			public const int lvNews = 2131099649;
 			
-			// aapt resource value: 0x7f06000c
-			public const int menu = 2131099660;
-			
-			// aapt resource value: 0x7f060018
-			public const int menuGetDataNow = 2131099672;
-			
-			// aapt resource value: 0x7f06001a
-			public const int menuMarkAllRead = 2131099674;
-			
-			// aapt resource value: 0x7f06000d
-			public const int menuMarkAsUnread = 2131099661;
+			// aapt resource value: 0x7f06000b
+			public const int menu = 2131099659;
 			
 			// aapt resource value: 0x7f060017
-			public const int menuSettings = 2131099671;
+			public const int menuGetDataNow = 2131099671;
 			
 			// aapt resource value: 0x7f060019
-			public const int menuShowReadNews = 2131099673;
+			public const int menuMarkAllRead = 2131099673;
+			
+			// aapt resource value: 0x7f06000c
+			public const int menuMarkAsUnread = 2131099660;
 			
 			// aapt resource value: 0x7f060016
-			public const int progressLoading = 2131099670;
+			public const int menuSettings = 2131099670;
 			
-			// aapt resource value: 0x7f060010
-			public const int scrollView1 = 2131099664;
+			// aapt resource value: 0x7f060018
+			public const int menuShowReadNews = 2131099672;
+			
+			// aapt resource value: 0x7f060015
+			public const int progressLoading = 2131099669;
+			
+			// aapt resource value: 0x7f06000f
+			public const int scrollView1 = 2131099663;
 			
 			// aapt resource value: 0x7f060000
 			public const int textView1 = 2131099648;
 			
-			// aapt resource value: 0x7f06000f
-			public const int tvDate = 2131099663;
-			
 			// aapt resource value: 0x7f06000e
-			public const int tvName = 2131099662;
+			public const int tvDate = 2131099662;
+			
+			// aapt resource value: 0x7f06000d
+			public const int tvName = 2131099661;
 			
 			// aapt resource value: 0x7f060009
 			public const int tvNewsDetail = 2131099657;
@@ -120,9 +120,6 @@ namespace de.dhoffmann.mono.adfcnewsapp.droid
 			
 			// aapt resource value: 0x7f060008
 			public const int tvNewsItemHeadline = 2131099656;
-			
-			// aapt resource value: 0x7f06000b
-			public const int wvNewsDetail = 2131099659;
 			
 			private Id()
 			{
